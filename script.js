a13b0dfc8b4d00251da1a1ee6931c6e9d8725e0d
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-10 01:16:25 +0200

    Update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Costs of items 
 const SHOES_COST = 300;
 const TOYS_COST = 100;
 const PENS_COST = 5;
@@ -10,33 +11,45 @@ const batteriesQuantity = 2;
 const toysQuantity = 5;
 const pensQuantity = 0; /* no pens were selected*/
 const shirtsQuantity = 0; /* no shirts were selected*/
-if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+// Calculate total cost of selected items:
+const totalCost =
+  SHOES_COST * shoesQuantity +
+  TOYS_COST * toysQuantity +
+  SHIRTS_COST * shirtsQuantity +
+  BATTERIES_COST * batteriesQuantity +
+  PENS_COST * pensQuantity; 
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+// Shipping costs and currency conditions;//
+if (customerLocation === 'RSA') {
+    shippingCost = 400;
+    currencySymbol = 'R';
+} else if (customerLocation === 'NAM') {
+    shippingCost = 600;
+    currencySymbol = 'USD';
+} else {
+    shippingCost = 800;
+    currencySymbol = 'USD';
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+// Warning if user is in North Korea
+if (customerLocation === 'NK') {
+    console.log('Unfortunately we do not curently ship to the selected country');
+} else {
+    console.log('Total Price', currencySymbol, totalCost + shippingCost);
+}
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+// Display a warning if shipping is free but there is more than 1 customer
+if (shippingCost === 0 && customers > 1) {
+    console.log('Free shipping only applies to single customer orders');
 
-customers = '1'
-const location = 'RSA'
-currency = null
+ // Check conditions for free shipping
+if ((totalCost >= 1000 && customerLocation === 'RSA') || (totalCost >= 60 && customerLocation === 'NAM' && customers === 1)) {
+    // Free shipping for total cost above 1000 in RSA or above 60 in NAM, and
+    // only 1 customer
+    shippingCost = 0;
+}
+
+// Set the value of customers and currencySymbol:
+let customers = 1;
+currencySymbol = null;
