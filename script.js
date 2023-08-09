a380d66e9527d5ca4da95507f2343a2f83c80a73
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-10 01:25:00 +0200

    Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Costs of items 
+// Costs of items
 const SHOES_COST = 300;
 const TOYS_COST = 100;
 const PENS_COST = 5;
@@ -18,36 +18,45 @@ const totalCost =
   TOYS_COST * toysQuantity +
   SHIRTS_COST * shirtsQuantity +
   BATTERIES_COST * batteriesQuantity +
-  PENS_COST * pensQuantity; 
+  PENS_COST * pensQuantity;
+
+let shippingCost = null;
+let currencySymbol = "$";
+
+const customerLocation = "RSA";
 
 // Shipping costs and currency conditions;//
-if (customerLocation === 'RSA') {
-    shippingCost = 400;
-    currencySymbol = 'R';
-} else if (customerLocation === 'NAM') {
-    shippingCost = 600;
-    currencySymbol = 'USD';
+if (customerLocation === "RSA") {
+  shippingCost = 400;
+  currencySymbol = "R";
+} else if (customerLocation === "NAM") {
+  shippingCost = 600;
+  currencySymbol = "USD";
 } else {
-    shippingCost = 800;
-    currencySymbol = 'USD';
+  shippingCost = 800;
+  currencySymbol = "USD";
 }
 
 // Warning if user is in North Korea
-if (customerLocation === 'NK') {
-    console.log('Unfortunately we do not curently ship to the selected country');
+if (customerLocation === "NK") {
+  console.log("Unfortunately we do not curently ship to the selected country");
 } else {
-    console.log('Total Price', currencySymbol, totalCost + shippingCost);
+  console.log("Total Price", currencySymbol, totalCost + shippingCost);
 }
 
 // Display a warning if shipping is free but there is more than 1 customer
 if (shippingCost === 0 && customers > 1) {
-    console.log('Free shipping only applies to single customer orders');
+  console.log("Free shipping only applies to single customer orders");
+}
 
- // Check conditions for free shipping
-if ((totalCost >= 1000 && customerLocation === 'RSA') || (totalCost >= 60 && customerLocation === 'NAM' && customers === 1)) {
-    // Free shipping for total cost above 1000 in RSA or above 60 in NAM, and
-    // only 1 customer
-    shippingCost = 0;
+// Check conditions for free shipping
+if (
+  (totalCost >= 1000 && customerLocation === "RSA") ||
+  (totalCost >= 60 && customerLocation === "NAM" && customers === 1)
+) {
+  // Free shipping for total cost above 1000 in RSA or above 60 in NAM, and
+  // only 1 customer
+  shippingCost = 0;
 }
 
 // Set the value of customers and currencySymbol:
